d4cc35dfff3fa69f74f4e6b7a35a955ca3a5f2f9
reggie01r <81575957+reggie01r@users.noreply.github.com>
2021-04-01 11:00:39 -0400

    First attempt
    Another attempt

---
+++ AnotherPracticeGithubAndMaven/src/main/java/com/Practice2/FirstPracticeMaven.java
@@ -0,0 +1,5 @@
+package com.Practice2;
+
+public class FirstPracticeMaven {
+
+}
---
+++ AnotherPracticeGithubAndMaven/src/test/java/com/Practice2/FirstPracticeMavenTest.java
@@ -0,0 +1,19 @@
+package com.Practice2;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class FirstPracticeMavenTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
